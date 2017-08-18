3b0100d200b9e202bd78e4a7b439da5cacf2f841
Admin <danny.garcia80@hotmail.com>
2017-08-18 15:50:44 -0700

    add business logic to scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,40 @@ $(document).ready(function() {
     var inputNum = $('input#number1').val();
     event.preventDefault();
 
-
+    outputAnswer(pongGen(inputNum));
 
   });
 });
+
+// Business Logic
+var pongGen = function(inputNum) {
+  var numberArr = [];
+  if (inputNum < 0) {
+    for (var i = -1; i >= inputNum; i--) {
+      numberArr.push(i);
+    }
+    numberArr.forEach(function(number) {
+      if (number % 15 === 0) {
+        numberArr[-number-1] = 'pingpong';
+      } else if (number % 3 === 0) {
+        numberArr[-number-1] = 'ping';
+      } else if (number % 5 === 0) {
+        numberArr[-number-1] = 'pong';
+      }
+    });
+  } else if (inputNum > 0) {
+      for (var i = 1; i <= inputNum; i++) {
+        numberArr.push(i);
+      }
+      numberArr.forEach(function(number) {
+        if (number % 15 === 0) {
+          numberArr[number-1] = 'pingpong';
+        } else if (number % 3 === 0) {
+          numberArr[number-1] = 'ping';
+        } else if (number % 5 === 0) {
+          numberArr[number-1] = 'pong';
+        }
+      });
+  }
+  return numberArr;
+};
