e751f44fa66312bc59e1085d428b890314b78e7d
Admin <danny.garcia80@hotmail.com>
2017-08-18 14:51:16 -0700

    edit scripts, jquery added

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// JQuery User Interface Logic
+$(document).ready(function() {
+  $('form#ping-pong').submit(function(event) {
+    var inputNum = $('input#number1').val();
+    event.preventDefault();
+
+
+
+  });
+});
